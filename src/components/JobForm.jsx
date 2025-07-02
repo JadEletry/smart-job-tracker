9adb91e4eab6801b289b47050507a44cd0b853a0
Jad Eletry <71851213+JadEletry@users.noreply.github.com>
2025-07-01 21:03:27 -0400

    Update JobForm.jsx

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -28,7 +28,7 @@ export default function JobForm({ onAddJob }) {
 
     const handleAnalyze = async () => {
         try {
-            const res = await fetch("http://localhost:5000/api/analyze", {
+            const res = await fetch("https://smart-job-tracker-kth8.onrender.com/api/analyze", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ jobDescription: form.jobDescription }),
